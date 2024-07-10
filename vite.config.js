829dd4146690793bc27fc9691e74cb992a16e826
Lafi Raed <browniesandmuffin@gmail.com>
2024-07-10 23:59:23 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: 'C:/Users/admin/Documents/VSC/recipe-app/src',
+  root: './src',
   server: {
     port: 3000,
     hot: true
-  },
-  resolve: {
-    alias: {
-      '~bootstrap':  'C:/Users/admin/Documents/VSC/recipe-app/node_modules/bootstrap'
-    }
   }
 })
