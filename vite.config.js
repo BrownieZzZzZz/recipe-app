4e626451f879af50fcbca8387756d0bb83feae3d
Lafi Raed <browniesandmuffin@gmail.com>
2024-07-11 00:06:18 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as dotenv from 'dotenv'
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   root: './src',
   server: {
-    port: 3000,
+    port: 3000 || process.env.PORT,
     hot: true
   }
 })
